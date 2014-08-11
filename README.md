e6b2d9d77f263d8d408968aaaff631f8537c8ced
Greg <stuppie@ufl.edu>
2014-08-11 15:07:43 -0400

    no message

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Tools for analyzing MALDI data of worms on slides.
 
-See maldiWorkflow.m for usgae instructions.
+See maldiWorkflow.m for usage instructions.
 
 Requires matlab bioinformatics toolbox and proteowizard's msconvert.
--- maldiWorkflow.m
+++ maldiWorkflow.m
@@ -3,8 +3,6 @@
 % Make sure you include vendor reader support (windows only).
 % Add the folder containing msconvert to your windows path. E.g. C:\Program Files (x86)\ProteoWizard\ProteoWizard 3.0.6452
 
-%Required files: 
-z
 %% Select all the raw files in the current directory
 d=dir('*.raw');
 d={d.name};
@@ -27,7 +25,7 @@ for i=1:length(d)
     [~,fname]=fileparts(d{i});
     msStruct=mzxmlread([fname '.mzXML']);
     [peaks,time] = mzxml2peaks(msStruct);
-    %remove peaks with m/z greater than 1000
+    %remove all m/z > 1000 (not the intensity!, the m/z range)
     peaks=cellfun(@(x) x(x(:,1)<1000,:), peaks, 'uniformoutput',false);
     AllPeaks = [AllPeaks; peaks];
     scanIdx = [scanIdx; repmat(i,length(peaks),1)];
@@ -52,7 +50,7 @@ end
 %
 %*** Change to change sample number
 sample=1;
-%% *** Build square array by averaging scans based on spot size
+%% *** Build square array by summing scans based on spot size
 windowSize = 2;
 spotID=spotIDs{sample}(:,[3,4]);
 spotID(:,1)=grp2idx(spotID(:,1));
@@ -70,7 +68,7 @@ end
 allPeaks=cat(2,sqPeaks{:}); %CMZ x scan
 sz=size(sqPeaks);
 %% plot mean of each scan
-% Decide if you want to normalize the total intensity of each pixel across all masses
+% **** Decide if you want to normalize the total intensity of each pixel across all masses*****
 imshow(reshape(mean(allPeaks,1),sz),[])
 
 %% Pull out image of a specific mass
@@ -104,7 +102,7 @@ y(segmentScans{3})=10;
 figure,imshow(y,[])
 
 %% Find masses related to sections
-[XL,~,XS,~,~,PCTVAR] = plsregress(zscore(allPeaks'),reshape(y,[],1),10);
+[~,~,~,~,~,PCTVAR] = plsregress(zscore(allPeaks'),reshape(y,[],1),10);
 numPC=find(cumsum(PCTVAR(1,:))>.90,1,'first');
 [XL,~,XS,~,BETA,PCTVAR] = plsregress(zscore(allPeaks'),reshape(y,[],1),numPC);
 scoresPlot=reshape(sum(XS(:,1:numPC),2),sz);
